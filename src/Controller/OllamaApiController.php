2f5c188b45556166142eebc8e319fb4e0c65e9aa
Lukáš Bečvář <lordbecvold@gmail.com>
2025-02-06 14:54:28 +0100

    added ollama api controller (with generate and get models list)

---
+++ src/Controller/OllamaApiController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use App\Util\AppUtil;
+use OpenApi\Attributes\Tag;
+use App\Manager\ErrorManager;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\RequestBody;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class OllamaApiController
+ *
+ * Controller for handling requests to Ollama API
+ *
+ * @package App\Controller
+ */
+class OllamaApiController extends AbstractController
+{
+    private AppUtil $appUtil;
+    private ErrorManager $errorManager;
+    private HttpClientInterface $httpClient;
+
+    public function __construct(AppUtil $appUtil, ErrorManager $errorManager, HttpClientInterface $httpClient)
+    {
+        $this->appUtil = $appUtil;
+        $this->httpClient = $httpClient;
+        $this->errorManager = $errorManager;
+    }
+
+    /**
+     * Get available models list from ollama
+     *
+     * @return JsonResponse List of available models from Ollama
+     */
+    #[Tag('ollama-api')]
+    #[Response(
+        response: JsonResponse::HTTP_OK,
+        description: 'List of available models from Ollama',
+        content: new JsonContent(
+            type: 'object',
+            properties: [
+                new Property(property: 'model_name', type: 'string', example: 'deepseek-r1:8b'),
+                new Property(property: 'status', type: 'string', example: 'available')
+            ]
+        )
+    )]
+    #[Route('/api/ollama/models', name: 'ollama_models', methods: ['GET'])]
+    public function getModelsList(): JsonResponse
+    {
+        try {
+            // send request to ollama api
+            $response = $this->httpClient->request('GET', $this->appUtil->getEnvValue('OLLAMA_API_URL') . '/api/tags');
+
+            // get response status code
+            $statusCode = $response->getStatusCode();
+            $data = $response->toArray(false);
+
+            // return response
+            return $this->json($data, $statusCode);
+        } catch (Exception $e) {
+            $this->errorManager->handleError(
+                message: 'Error to get models list from Ollama API',
+                code: JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                exceptionMessage: $e->getMessage()
+            );
+        }
+    }
+
+    /**
+     * Execute prompt using the Ollama API
+     *
+     * @param Request $request Request object
+     *
+     * @return JsonResponse Response from Ollama API
+     */
+    #[Response(
+        response: JsonResponse::HTTP_OK,
+        description: 'Successfully proxied the request to Ollama API',
+        content: new JsonContent(
+            type: 'object',
+            properties: [
+                new Property(property: 'response', type: 'object', description: 'The response from Ollama API')
+            ]
+        )
+    )]
+    #[Response(
+        response: JsonResponse::HTTP_BAD_REQUEST,
+        description: 'Bad Request - Missing required parameters',
+        content: new JsonContent(
+            type: 'object',
+            properties: [
+                new Property(property: 'status', type: 'string', example: 'error'),
+                new Property(property: 'message', type: 'string', example: 'Missing required parameters')
+            ]
+        )
+    )]
+    #[RequestBody(
+        content: new JsonContent(
+            type: 'object',
+            properties: [
+                new Property(property: 'model', type: 'string', example: 'deepseek-r1:8b'),
+                new Property(property: 'prompt', type: 'string', example: 'Hello, how are you?'),
+                new Property(property: 'stream', type: 'boolean', example: false)
+            ]
+        )
+    )]
+    #[Tag('ollama-api')]
+    #[Route('/api/ollama/prompt', name: 'ollama_proxy', methods: ['POST'])]
+    public function executePrompt(Request $request): JsonResponse
+    {
+        // get data from request
+        $content = json_decode($request->getContent(), true);
+
+        // check if required parameters are set
+        if (!isset($content['model']) || !isset($content['prompt'])) {
+            return $this->json([
+                'status' => 'error',
+                'message' => 'Missing required parameters'
+            ], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // send request to ollama api
+        $response = $this->httpClient->request('POST', $this->appUtil->getEnvValue('OLLAMA_API_URL') . '/api/generate', [
+            'json' => $content,
+        ]);
+
+        // get response status code
+        $statusCode = $response->getStatusCode();
+        $data = $response->toArray(false);
+
+        // return response
+        return new JsonResponse(json_encode($data, JSON_UNESCAPED_SLASHES), $statusCode, json: true);
+    }
+}
---
+++ tests/Controller/OllamaApiControllerTest.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * Class IndexControllerTest
+ *
+ * Test cases for ollama api proxy controller
+ *
+ * @package App\Tests\Controller
+ */
+class OllamaApiControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * Test get models list when request method is not valid
+     *
+     * @return void
+     */
+    public function testGetModelsListWhenRequestMethodIsNotValid(): void
+    {
+        $this->client->request('POST', '/api/ollama/models');
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('error', $responseData['status']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * Test get models list when api token is not provided
+     *
+     * @return void
+     */
+    public function testGetModelsListWhenApiTokenIsNotProvided(): void
+    {
+        $this->client->request('GET', '/api/ollama/models');
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('Invalid access token.', $responseData['message']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Test get models list when api token is invalid
+     *
+     * @return void
+     */
+    public function testGetModelsListWhenApiTokenIsInvalid(): void
+    {
+        $this->client->request('GET', '/api/ollama/models', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_X_API_TOKEN' => 'invalid-token',
+        ]);
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('Invalid access token.', $responseData['message']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Test get models list with success response
+     *
+     * @return void
+     */
+    public function testGetModelsListWithSuccessResponse(): void
+    {
+        // mock ollama api response
+        $mockResponse = $this->createMock(ResponseInterface::class);
+        $mockResponse->method('getStatusCode')->willReturn(JsonResponse::HTTP_OK);
+        $mockHttpClient = $this->createMock(HttpClientInterface::class);
+        $mockHttpClient->expects($this->once())->method('request')->willReturn($mockResponse);
+        $this->client->getContainer()->set(HttpClientInterface::class, $mockHttpClient);
+
+        // send request to models list controller
+        $this->client->request('GET', '/api/ollama/models', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_X_API_TOKEN' => $_ENV['API_TOKEN'],
+        ]);
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Test execute prompt when request method is not valid
+     *
+     * @return void
+     */
+    public function testExecutePromptWhenRequestMethodIsNotValid(): void
+    {
+        $this->client->request('GET', '/api/ollama/prompt');
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('error', $responseData['status']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * Test execute prompt when api token is not provided
+     *
+     * @return void
+     */
+    public function testExecutePromptWhenApiTokenIsNotProvided(): void
+    {
+        $this->client->request('POST', '/api/ollama/prompt', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ]);
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('Invalid access token.', $responseData['message']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Test execute prompt when api token is invalid
+     *
+     * @return void
+     */
+    public function testExecutePromptWhenApiTokenIsInvalid(): void
+    {
+        $this->client->request('POST', '/api/ollama/prompt', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_X_API_TOKEN' => 'invalid-token',
+        ]);
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('Invalid access token.', $responseData['message']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Test execute prompt when request parameters is missing
+     *
+     * @return void
+     */
+    public function testExecutePromptWhenRequestParametersIsMissing(): void
+    {
+        $this->client->request('POST', '/api/ollama/prompt', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_X_API_TOKEN' => $_ENV['API_TOKEN'],
+        ]);
+
+        /** @var array<mixed> $responseData */
+        $responseData = json_decode(($this->client->getResponse()->getContent() ?: '{}'), true);
+
+        // assert response
+        $this->assertSame('error', $responseData['status']);
+        $this->assertSame('Missing required parameters', $responseData['message']);
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Test execute prompt with success response
+     *
+     * @return void
+     */
+    public function testExecutePromptWithSuccessResponse(): void
+    {
+        // mock ollama api response
+        $mockResponse = $this->createMock(ResponseInterface::class);
+        $mockResponse->method('getStatusCode')->willReturn(JsonResponse::HTTP_OK);
+        $mockHttpClient = $this->createMock(HttpClientInterface::class);
+        $mockHttpClient->expects($this->once())->method('request')->willReturn($mockResponse);
+        $this->client->getContainer()->set(HttpClientInterface::class, $mockHttpClient);
+
+        // send request to prompt execute controller
+        $this->client->request('POST', '/api/ollama/prompt', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_X_API_TOKEN' => $_ENV['API_TOKEN']
+        ], json_encode([
+            'model' => 'deepseek-r1:8b',
+            'prompt' => 'Hi, how are you?',
+            'stream' => false
+        ]) ?: null);
+
+        // assert response
+        $this->assertResponseStatusCodeSame(JsonResponse::HTTP_OK);
+    }
+}
